bc7da29eab86cf063a7cb1441c0b61ec879ba9a4
goodmixLOL <goodmix007@gmail.com>
2016-03-01 18:09:00 +0300

    updates

---
+++ Some Algorithms/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Algorithms
+{
+    class Algorithms
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int temp = 0;
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++) Console.WriteLine(arr[i]);
+
+            
+        }
+    
+    }
+}
--- Some Algorithms/Fibonacci.cs
+++ Some Algorithms/Fibonacci.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Some_Algorithms
 {
-    class Program
+    class Fibonacci
     {
-        public static int Fibonacci(int n)
+        public static int FibonacciFunc(int n)
         {
+     
             int a = 1;
             int b = 1;
             // In N steps compute Fibonacci sequence iteratively.
@@ -24,11 +25,26 @@ namespace Some_Algorithms
 
         static void Main()
         {
+            /*
             for (int i = 0; i < 15; i++)
             {
-                Console.WriteLine(Fibonacci(i));
+                Console.WriteLine(FibonacciFunc(i));
             }
             Console.Read();
+            */
+
+            Console.WriteLine("Length of array = ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+           
+            for(int i = 0; i < length; i ++)
+            {
+                Console.WriteLine("Populate the array: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Algorithms.BubbleSort(array);
+
+            Console.ReadLine();
         }
     }
 }
